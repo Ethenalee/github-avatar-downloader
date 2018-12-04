9fce02912542680b2456ec993f661c99d571a7cd
Ethena lee <yeonji.900111@gmail.com>
2018-12-04 03:26:38 +0000

    done file path, url, env missing error

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,4 @@
+console.log('Running file');
 require('dotenv').config();
 
 var request = require('request');
@@ -5,19 +6,32 @@ var key = process.env.GITHUB_TOKEN;
 var fs = require('fs');
 var owner = process.argv[2];
 var repo = process.argv[3];
+var filepath = './avatars/';
 
 // get API
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
     headers: {
       'User-Agent': 'request',
       Authorization: 'token ' + key
     }
   };
-  if(repoOwner === undefined || repoName === undefined) {
-    return console.log("Errors: argument is missing");
+
+  if(!fs.existsSync(key)) {
+    return console.log('Errors: .env is missing');
   }
+
+  if(!fs.)
+
+  if(!fs.existsSync(options.url)) {
+    return console.log('Errors: url is missing');
+  }
+
+  // if(!response || response != 200) {
+  //   console.log('Errors', )
+  // }
+
   request(options, function(err, res, raw) {
     var body = JSON.parse(raw);
     cb(err, body);
@@ -25,22 +39,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 // print avatar url
-var cab = function(err, result) {
+var cb1 = function(err, result) {
   if(err) {
-    console.log("Errors: ", err);
+    console.log('Errors: ', err);
   }
   for(var i = 0; i < result.length; i ++) {
-  console.log("Result: ", result[i].avatar_url);
+  console.log('Result: ', result[i].avatar_url);
   }
 };
 
 // save image in avatars folder
 var cb2 = function(err, result) {
   if(err) {
-    console.log("Errors: ", err);
+    console.log('Errors: ', err);
   }
+
+  if(!fs.existsSync(filepath)) {
+    return console.log('Errors: filePath is missing');
+  }
+
   for(var i = 0; i < result.length; i ++) {
-   downloadImageByURL(result[i].avatar_url, './avatars/' + result[i].login + '.png');
+   downloadImageByURL(result[i].avatar_url, filepath + result[i].login + '.png');
   }
 }
 
@@ -49,6 +68,7 @@ getRepoContributors(owner, repo, cb2);
 
 
 function downloadImageByURL(url, filePath) {
+
   request.get(url)
          .pipe(fs.createWriteStream(filePath))
 
